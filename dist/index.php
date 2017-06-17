ed49d494b0aac430e4e8030cc82398b6c5ada4ab
alex vaught <alex@nuevecubica.net>
2017-06-17 10:30:58 -0500

    commit with an error

---
+++ dist/index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>:: Title ::</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="web page">
+    <meta name="author" content="author">
+    <!--[if lt IE 9]>
+    <script src="https://cdnjsgit add.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+    <!-- BOOTSTRAP Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- GENERALS CSS -->
+    <link rel="stylesheet" href="assets/css/generals.min.css">
+</head>
+<body>
+<div class="app-holder">
+bababaoiuhuhd
+    <article>
+        <main>
+            <header>
+                <h1>HTML5 Boilerplate</h1>
+                <figure>
+                    <img src="" alt="">
+                    <figcaption>People who liked the article</figcaption>
+                </figure>
+                <nav class="menu-horizontal">
+                    <ul>
+                        <li>menu 1</li>
+                        <li>menu 2</li>
+                        <li>menu 3</li>
+                        <li class="active">menu 4</li>
+                        <li>menu 5</li>
+                    </ul>
+                </nav>
+                <span class="row"></span>
+                <nav class="menu-vertical">
+                    <ul>
+                        <li>menu 1</li>
+                        <li>menu 2</li>
+                        <li>menu 3</li>
+                        <li class="active">menu 4</li>
+                        <li>menu 5</li>
+                    </ul>
+                </nav>
+            </header>
+            <section>
+
+                <h2> sub titles</h2>
+                <p>just text <strong>denting force strong</strong></p>
+                <cite>this is a simple cite </cite>
+
+            </section>
+            <section>
+                <h2>Semantic HTML </h2>
+                <p>just text <strong>denting force strong</strong></p>
+                <cite>this is a simple cite </cite>
+                <time datetime="2009-10-10T19:10-08:00">~1 hour ago</time>
+                <i>this is a tecnical term</i>
+                <b>boldy tyepe</b>
+                <em>this is with emphasis</em>
+            </section>
+
+
+            <aside>
+                <p> Aside non important to the article</p>
+            </aside>
+            <footer>
+                <menu>
+                    <p>footer menu</p>
+                    <small>small type.....</small>
+                </menu>
+            </footer>
+        </main>
+    </article>
+</div>
+<!-- JQUERY -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!--  BOOTSTRAP Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+<!-- GENERALS JS-->
+<script src="assets/scripts/generals.min.js"></script>
+</body>
+</html>
+
+
