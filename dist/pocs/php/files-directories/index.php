8c988b7572bf207574686d300dc4151cc034d91b
alex vaught <alex@nuevecubica.net>
2017-07-11 22:42:00 -0500

    11-07-17 from macbook

--- dist/pocs/php/files-directories/index.php
+++ dist/pocs/php/files-directories/index.php
@@ -178,7 +178,7 @@ include_layout_element('poc_header.php');
         </code>
         </pre>
         <div class="spacer"></div>
-        <h2>Accesing Files</h2>
+        <h2>Accessing Files</h2>
         <p>fopen(filename,mode) creates or edit a file depending of the permissions</p>
         <table>
             <tr>
@@ -225,17 +225,195 @@ include_layout_element('poc_header.php');
         <p><strong>b</strong> Binary Mode (don't translate)</p>
         <p><strong>b</strong> is the deffault since php 4.3.2</p>
 
+        <div class="spacer"></div>
+        <h2>File Write</h2>
+        <pre>
+            <code class="PHP">
+
+        // fopen writes a file and ands a message
+        $file = 'filetest.txt';
+        // REMEMBER to use double quotes for using \n line brakes
+        $message = "this is a message fro a var\n";
+        $message2 = date("d/M/Y H:i:s\n");
+        $content = $message .  $message2;
+
+        if ($handle = fopen($file, 'w')) { // OVERWRITE
+            fwrite($handle, $content);
+            chmod($file, 0777);
+            echo $file . ' is written';
+            fclose($handle);
+            echo 'File is close';
+        } else {
+            echo 'Could not open file for writting';
+        }
+        // SHORCUT FOR fopen/fwrite/fclose OVERWRITES EXISTING FILE by DEFAULT
+        if ($size = file_put_contents($file, $content)) {
+            echo "a file of {$size} bytes was created,";
+        }
+
+            </code>
+        </pre>
+
         <?php
 
         $file = 'filetest.txt';
+        $message = "this is a message from a var\n";
+        $message2 = date("d/M/Y H:i:s\n");
+        $content = $message . $message2;
 
-        if($handle = fopen($file,'w')){
+        if ($handle = fopen($file, 'w')) { // OVERWRITE
+            fwrite($handle, $content);
+            chmod($file, 0777);
+            echo '<p style="color:mediumseagreen">' . $file . ' is written</p>';
             fclose($handle);
-        }else{
+            echo '<p style="color:orange">File is close</p>';
+        } else {
             echo '<p style="color:red">Could not open file for writting</p>';
         }
+        // SHORCUT FOR fope/fwrite/fclose OVERWRITES EXISTING FILE by DEFAULT
+        if ($size = file_put_contents($file, $content)) {
+            echo "a file of {$size} bytes was created,";
+        }
+        ?>
+        <div class="spacer"></div>
+        <h2>Delete Files</h2>
+
+        <p>Close files first</p>
+        <ul>
+            <li>Can´t delete open files</li>
+            <li>Must have permission on folder containing the file</li>
+            <li>USE unlink();</li>
+        </ul>
+        <div class="spacer"></div>
+        <pre>
+            <code class="PHP">
+                 // DELETE FILE
+                  unlink($file);
+
+            </code>
+        </pre>
+        <div class="spacer"></div>
+        <h2>Move the cursos</h2>
+        <p>Use ftell() to get the position of the cursor</p>
+        <?php
+        $file = 'filetest2.txt';
+        $message = "this is a message from a var\n";
+        $message2 = date("d/M/Y H:i:s\n");
+        $message3 = "en el offset con un break\n";
+        $message4 = "inicio\n";
+        $content = $message . $message2;
+
+        if ($handle = fopen($file, 'w')) { // OVERWRITE
+            fwrite($handle, $content);
+            chmod($file, 0777);
+            $pos = ftell($handle);
+            fseek($handle, $pos - 21);
+            fwrite($handle, $message3);
+            rewind($handle);
+            fwrite($handle, $message4);
+            echo '<p style="color:mediumseagreen">' . $file . ' is written</p>';
+            fclose($handle);
+            echo '<p style="color:orange">File is close</p>';
+        } else {
+            echo '<p style="color:red">Could not open file for writting</p>';
+        } // BEWARE THIS WILL OVERTYPE!!!!
+        ?>
+        <div class="spacer"></div>
+        <pre>
+            <code class="php">
+        // BEWARE THIS WILL OVERTYPE!!!!
+        // NOTE a and a+ WILL NOT let you move the pointer
+
+        $file = 'filetest2.txt';
+        $message = "this is a message from a var\n";
+        $message2 = date("d/M/Y H:i:s\n");
+        $message3 = "en el offset con un break\n";
+        $message4 = "inicio\n";
+        $content = $message . $message2;
+
+        if ($handle = fopen($file, 'w')) { // OVERWRITE
+            fwrite($handle, $content);
+            chmod($file, 0777);
+            $pos = ftell($handle);
+            fseek($handle,$pos-21);
+            fwrite($handle,$message3);
+            rewind($handle);
+            fwrite($handle,$message4);
+            echo $file . ' is written';
+            fclose($handle);
+            echo 'File is close';
+        } else {
+            echo 'Could not open file for writting';
+        }
+            </code>
+        </pre>
+        <div class="spacer"></div>
+        <h2>Reading Files</h2>
+        <ul>
+            <li>fopen with option r</li>
+            <li>using fread()</li>
+        </ul>
+        <?php
+        $file_read = 'filetest.txt';
+        if ($handle = fopen($file_read, 'r')) { // READ
+            // $content = fread($handle,30); // in english each character is 1 byte;
+            $content = fread($handle, filesize($file_read)); // get the full size of the file;
+            fclose($handle);
+            //  \n to <br>
+            echo nl2br($content);
+            echo '<br>';
+            // SHORTCUT
+            $content = file_get_contents($file_read);
+            echo nl2br($content);
 
 
+        }
+        ?>
+        <div class="spacer"></div>
+        <pre>
+            <code class="php">
+
+        $file_read = 'filetest.txt';
+        if ($handle = fopen($file_read,'r')){ // READ
+           // $content = fread($handle,30); // each character is 1 byte;
+            $content = fread($handle,filesize($file_read)); // get the full size of the file;
+            fclose($handle);
+            //  \n to html br
+            echo nl2br($content);
+
+            // SHORT CUT
+            $content = file_get_contents($file_read);
+            echo nl2br($content);
+            </code>
+        </pre>
+        <div class="spacer"></div>
+        <h2>Incremental Reading</h2>
+        <p></p>
+        <pre>
+            <code class="php">
+
+        $file_read = 'filetest.txt';
+        $content = "";
+        if ($handle = fopen($file_read, 'r')) { // READ
+            while(!feof($handle)){
+                $content .= fgets($handle);
+            }
+            fclose($handle);
+            echo nl2br($content);
+        }
+
+            </code>
+        </pre>
+        <?php
+        $file_read = 'filetest.txt';
+        $content = "";
+        if ($handle = fopen($file_read, 'r')) { // READ
+            while(!feof($handle)){
+                $content .= fgets($handle);
+            }
+            fclose($handle);
+            echo nl2br($content);
+        }
         ?>
     </article>
 </div>
@@ -250,7 +428,7 @@ include_layout_element('poc_footer.php');
     });
 
 
-    for(var i = 0; i <= 10; i++)
+    for (var i = 0; i <= 10; i++)
         console.log(i);
 
 </script>
